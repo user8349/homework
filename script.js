e4bbf665e036136192d7c5a78dae67a5ff26ae84
feros.pro <ksbukreev@feros.pro>
2023-06-22 12:35:18 +0500

    hw-1

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/**
+ * Author:  Konstantin Bukreev
+ * Date:    22.06.2023 12:34
+ * Title:   01-js-base-task
+ */
+
 const a = [3, 2, 1]
 const b = [3, 4, 2, 5]
 const intersect = (arr1, arr2) => {
